f69c99e1d6698141566a301ecd669ad02693e615
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-06-04 14:41:38 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,10 +1,10 @@
-g=int(input())
+gm=int(input())
 fto=1
-if(g<0):
+if(gm<0):
     print("invalid")
-elif(g<=1):
+elif(gm<=1):
     print(1)
 else:
-    for i in range(1,g+1):
+    for i in range(1,gm+1):
         fto=fto*i
     print(fto)
