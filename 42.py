ce5b0dd3072989c8cdcc1036565e345464b23b90
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-06-07 10:43:25 +0530

    Update 42.py

--- 42.py
+++ 42.py
@@ -1,5 +1,12 @@
-q=input()
-q=q.split()
-k=list(map(int,q))
-r=max(k)
-print(r)
+e=input()
+e=e.split()
+y=e[0]
+t=e[1]
+r=len(str(y))
+l=len(str(t))
+if(r<l):
+    print(t)
+elif(l<r):
+    print(y)
+else:
+    print(y or t)
