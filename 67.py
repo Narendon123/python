4951e825288558d60e23a6b6054c04f479334616
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-06-10 15:10:47 +0530

    Create 67.py

---
+++ 67.py
@@ -0,0 +1,14 @@
+l=int(input())
+if(l>0 and l<=10):
+    print(10)
+elif(l>=11 and l<=20):
+    print(20)
+elif(l>=21 and l<=30):
+    print(30)
+elif(l>=31 and l<=40):
+    print(40)
+elif(l>=41 and l<=50):
+    print(50)
+elif(l>=51 and l<=60):
+    print(60)
+                          
