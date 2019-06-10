3a91f512f44a45c88f6cb4eb6bc05c97dcb4b4ca
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-06-10 09:49:35 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,30 @@
+r=int(input())
+if(r==1):
+    print("One")
+elif(r==2):
+    print("Two")
+elif(r==3):
+    print("Three")
+elif(r==4):
+    print("Four")
+elif(r==5):
+    print("Five")
+elif(r==6):
+    print("Six")
+elif(r==7):
+    print("Seven")
+elif(r==8):
+    print("Eight")
+elif(r==9):
+    print("Nine")
+elif(r==10):
+    print("Ten")
+    
+        
+        
+        
+        
+        
+        
+        
+        
