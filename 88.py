f2eb743e41d51f075b597315d96f3759f0a545df
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-06-11 12:46:21 +0530

    Create 88.py

---
+++ 88.py
@@ -0,0 +1,14 @@
+w=input()
+w=w.split()
+a=int(w[0])
+b=int(w[1])
+i=1
+if(a>b):
+    min1=a
+else:
+    min1=b
+while(1):
+    if(min1%i==0 and min1%i==0):
+        print(min1)
+        break
+    min1=min1+1
