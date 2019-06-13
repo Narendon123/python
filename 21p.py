e6cae4dc0bcb4032e4956bddf3805d470091f6d5
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-06-13 15:09:33 +0530

    Update 21p.py

--- 21p.py
+++ 21p.py
@@ -1,7 +1,7 @@
 w=input().split()
 w2=input().split()
 w3=input().split()
-if(w[0]==w2[0]==w3[0] or w[1]==w2[1]==w3[1]):
+if(w[0]==w2[0]==w3[0] or w[1]==w2[1]==w3[1] or (w[0]==w[1] and w2[0]==w2[1] and w3[0]==w3[1])):
     print("yes")
 else:
     print("no")
