895ae7bafe18ca145c2d8e880adaeac83dce3679
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-06-14 10:01:01 +0530

    Create 11p.py

---
+++ 11p.py
@@ -0,0 +1,11 @@
+r=input()
+r2=input()
+if(r=="Sunday" or r=="Saturday"):
+    print("yes")
+else:
+    print("no")
+if(r2=="Sunday" or r2=="Saturday"):
+    print("yes")
+else:
+    print("no")
+    
