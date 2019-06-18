01b37bdc6982bb824e5d9f8078747be288fbeb5d
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-06-18 10:22:31 +0530

    Update 63p.py

--- 63p.py
+++ 63p.py
@@ -8,7 +8,8 @@ for i in w:
     l.append(i)
 for i in w2:
     l2.append(i)
-for i in range(0,len(l)):
-    if(l[i]==l2[i]):
+for i in range(0,e):
+    for j in range(0,e):
+       if(l[i]==l2[j]):
         l3.append(l[i])
 print(*l3,sep=" ")
