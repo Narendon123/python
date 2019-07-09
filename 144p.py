d0ee31c8a5e68e6d573149f2deeba6930b725bc0
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-07-09 18:15:33 +0530

    Update 144p.py

--- 144p.py
+++ 144p.py
@@ -1,7 +1,7 @@
 s=int(input())
 w=list(map(int,input().split()))
 g=[]
-for i in range(0,s+1):
-  if(w[i+1]%w[i]==0):
+for i in range(0,s-1):
+  if(w[i-1]%w[i]==0):
     g.append(w[i+1])
 print(g)    
