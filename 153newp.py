67d0ee4d0130cfda78311e45987afe2cb87d5a6b
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-07-10 18:01:53 +0530

    Create 153newp.py

---
+++ 153newp.py
@@ -0,0 +1,17 @@
+q=input()
+q=q.split()
+q1=str(q[0])
+q2=int(q[1])
+l=[]
+l2=[]
+for i in q1:
+    l.append(i)
+a=len(l)
+if(q2==1):
+    print(l[0])
+else:    
+    for i in range(0,a,q2):
+        l2.append(l[i+1])
+    print(*l2)    
+
+        
